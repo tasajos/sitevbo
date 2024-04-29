f02eb2dc3fa71f38e23dbeac309d96b9039f42e7
tasajos <tasajos@gmail.com>
2024-04-28 20:18:56 -0400

    update sitevbo

---
+++ sitevbo/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import logo from './logo.svg';
+import Principal from './Components/Principal/Principal';
+
+function App() {
+  return (
+    <Router>
+      
+        <Routes>
+          <Route path="/" element={<Principal />} />
+          {/* Aquí puedes añadir más rutas según sea necesario */}
+        </Routes>
+      
+    </Router>
+  );
+}
+
+export default App;
---
+++ sitevbo/src/Components/Principal/Principal.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Principal.css';  // Importa los estilos específicos
+
+function Principal() {
+    return (
+        <div className="principal">
+            <h1>Bienvenidos a la Plataforma de Voluntarios de Bolivia</h1>
+            <p>Explora las campañas activas o inicia tu propia campaña.</p>
+            {/* Aquí podrás añadir más componentes como listas de campañas, etc. */}
+        </div>
+    );
+}
+
+export default Principal;
