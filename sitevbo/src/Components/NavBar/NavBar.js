e3dabfca3469fc7b5bdd6d13766d9439e50594f1
tasajos <tasajos@gmail.com>
2024-04-28 21:02:30 -0400

    update menus

---
+++ sitevbo/src/Components/NavBar/NavBar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './NavBar.css'; // Asegúrate de que el nombre del archivo coincida
+import logo from '../Imagenes/Voluntarios.png'; // Asegúrate de tener una imagen de logo
+
+const NavBar = () => {
+    return (
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">
+          <img src={logo} height="40" className="d-inline-block align-top" alt="Logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          {/* Mueve el Form antes de Nav para que se alinee a la izquierda */}
+          <Form className="align-left">
+            <FormControl type="search" placeholder="Buscar" className="mr-sm-2" />
+          </Form>
+          {/* Los elementos Nav seguirán alineados a la derecha */}
+          <Nav className="ml-auto">
+          <LinkContainer to="/descubrir">
+            <Nav.Link>Descubrir</Nav.Link>
+          </LinkContainer>
+          <NavDropdown title="Recaudar fondos para" id="basic-nav-dropdown">
+            <LinkContainer to="/causa1">
+              <NavDropdown.Item>Causa 1</NavDropdown.Item>
+            </LinkContainer>
+            {/* Más ítems aquí */}
+          </NavDropdown>
+          <Nav.Link href="#how-it-works">Cómo funciona</Nav.Link>
+          <LinkContainer to="/iniciar-sesion">
+            <Nav.Link>Iniciar sesión</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/iniciar-campania">
+            <Button variant="outline-success">Iniciar una campaña</Button>
+          </LinkContainer>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default NavBar;
--- sitevbo/src/Components/Principal/Principal.js
+++ sitevbo/src/Components/Principal/Principal.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './Principal.css';  // Importa los estilos específicos
+import NavBar from '../NavBar/NavBar'; // Ajusta la ruta relativa según sea necesario
 
 function Principal() {
     return (
+
+        <div>
+            <NavBar />
         <div className="principal">
             <h1>Bienvenidos a la Plataforma de Voluntarios de Bolivia</h1>
             <p>Explora las campañas activas o inicia tu propia campaña.</p>
             {/* Aquí podrás añadir más componentes como listas de campañas, etc. */}
+        </div>
         </div>
     );
 }
